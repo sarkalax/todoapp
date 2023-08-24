65b61736b01c0c956a47236ca65274bab87299d3
sarysx <112681783+sarysx@users.noreply.github.com>
2023-08-24 12:55:01 +0200

    male upravy

--- src/Components/AddForm.jsx
+++ src/Components/AddForm.jsx
@@ -7,149 +7,146 @@ import { TaskList } from "./TaskList";
 import { RadioInput } from "./RadioInput";
 
 export function AddForm() {
-const [nameOfTask, setNameOfTask] = useState("");
-const [tasks, setTasks] = useState(() => {
-    const localValue = localStorage.getItem("TASKS");
-    if (localValue === null) return [];
-    return JSON.parse(localValue);
-});
-const [selectedList, setSelectedList] = useState("");
+    const [nameOfTask, setNameOfTask] = useState("");
+    const [tasks, setTasks] = useState(() => {
+        const localValue = localStorage.getItem("TASKS");
+        if (localValue === null) return [];
+        return JSON.parse(localValue);
+    });
+    const [selectedList, setSelectedList] = useState("");
 
-const [homeTasks, setHomeTasks] = useState([]);
-const [workTasks, setWorkTasks] = useState([]);
-const [schoolTasks, setSchoolTasks] = useState([]);
-const [otherTasks, setOtherTasks] = useState([]);
+    const [homeTasks, setHomeTasks] = useState([]);
+    const [workTasks, setWorkTasks] = useState([]);
+    const [schoolTasks, setSchoolTasks] = useState([]);
+    const [otherTasks, setOtherTasks] = useState([]);
 
-function addTask(nameOfTask, selectedList) {
-    if (nameOfTask === "") return;
+    function addTask(nameOfTask, selectedList) {
+        if (nameOfTask === "") return;
 
-    setTasks((tasks) => [
-        ...tasks,
-        {
-            title: nameOfTask,
-            id: crypto.randomUUID(),
-            checked: false,
-            type: selectedList,
-        },
-    ]);
+        setTasks((tasks) => [
+            ...tasks,
+            {
+                title: nameOfTask,
+                id: crypto.randomUUID(),
+                checked: false,
+                type: selectedList,
+            },
+        ]);
 
-    setNameOfTask("");
-}
+        setNameOfTask("");
+    }
 
-function deleteTask(parId) {
-    setTasks((tasks) => tasks.filter((task) => task.id !== parId));
-}
+    function deleteTask(parId) {
+        setTasks((tasks) => tasks.filter((task) => task.id !== parId));
+    }
 
-useEffect(() => {
-    localStorage.setItem("TASKS", JSON.stringify(tasks));
+    useEffect(() => {
+        localStorage.setItem("TASKS", JSON.stringify(tasks));
 
-    setHomeTasks(
-        (homeTasks) =>
-            (homeTasks = tasks.filter((task) => task.type === "home"))
-    );
-    setWorkTasks(
-        (workTasks) =>
-            (workTasks = tasks.filter((task) => task.type === "work"))
-    );
-    setSchoolTasks(
-        (schoolTasks) =>
-            (schoolTasks = tasks.filter((task) => task.type === "school"))
-    );
-    setOtherTasks((otherTasks) =>
-        tasks.filter((task) => task.type === "other")
-    );
-}, [tasks]);
+        setHomeTasks((homeTasks) =>
+            tasks.filter((task) => task.type === "home")
+        );
+        setWorkTasks((workTasks) =>
+            tasks.filter((task) => task.type === "work")
+        );
+        setSchoolTasks((schoolTasks) =>
+            tasks.filter((task) => task.type === "school")
+        );
+        setOtherTasks((otherTasks) =>
+            tasks.filter((task) => task.type === "other")
+        );
+    }, [tasks]);
 
-return (
-    <>
-        <section className="lists">
-            <section className="taskForm glassEffect">
-                <form
-                    autoComplete="off"
-                    onSubmit={(e) => e.preventDefault()}
-                >
-                    <h2>To Do App</h2>
-                    <div className="searchDiv">
-                        <input
-                            type="text"
-                            name="inputText"
-                            value={nameOfTask}
-                            onChange={(e) => setNameOfTask(e.target.value)}
-                        />
-                        <button
-                            className="defaultBttn"
-                            onClick={() =>
-                                addTask(nameOfTask, selectedList)
-                            }
-                        >
-                            Add
-                        </button>
-                    </div>
-                    <div className="radioSection">
-                        <RadioInput
-                            value={"work"}
-                            setSelectedList={setSelectedList}
-                        />
-                        <RadioInput
-                            value={"home"}
-                            setSelectedList={setSelectedList}
-                        />
-                        <RadioInput
-                            value={"school"}
-                            setSelectedList={setSelectedList}
-                        />
-                        <RadioInput
-                            value={"other"}
-                            setSelectedList={setSelectedList}
-                        />
-                    </div>
-                </form>
-                <section className="allTasks">
-                    <h3>All</h3>
-                    <ul>
-                        {tasks.map((task) => {
-                            const { title, id } = task;
+    return (
+        <>
+            <section className="lists">
+                <section className="taskForm glassEffect">
+                    <form
+                        autoComplete="off"
+                        onSubmit={(e) => e.preventDefault()}
+                    >
+                        <h2>To Do App</h2>
+                        <div className="searchDiv">
+                            <input
+                                type="text"
+                                name="inputText"
+                                value={nameOfTask}
+                                onChange={(e) => setNameOfTask(e.target.value)}
+                            />
+                            <button
+                                className="defaultBttn"
+                                onClick={() =>
+                                    addTask(nameOfTask, selectedList)
+                                }
+                            >
+                                Add
+                            </button>
+                        </div>
+                        <div className="radioSection">
+                            <RadioInput
+                                value={"work"}
+                                setSelectedList={setSelectedList}
+                            />
+                            <RadioInput
+                                value={"home"}
+                                setSelectedList={setSelectedList}
+                            />
+                            <RadioInput
+                                value={"school"}
+                                setSelectedList={setSelectedList}
+                            />
+                            <RadioInput
+                                value={"other"}
+                                setSelectedList={setSelectedList}
+                            />
+                        </div>
+                    </form>
+                    <section className="allTasks">
+                        <h3>All</h3>
+                        <ul>
+                            {tasks.map((task) => {
+                                const { title, id } = task;
 
-                            return (
-                                <li key={id} onClick={() => deleteTask(id)}>
-                                    {title}
-                                    <button className="checkIcon">
-                                        <FaCheck />
-                                    </button>
-                                </li>
-                            );
-                        })}
-                    </ul>
+                                return (
+                                    <li key={id} onClick={() => deleteTask(id)}>
+                                        {title}
+                                        <button className="checkIcon">
+                                            <FaCheck />
+                                        </button>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </section>
+                    <button
+                        className="defaultBttn deleteAllBttn"
+                        onClick={() => setTasks([])}
+                    >
+                        Delete All
+                    </button>
                 </section>
-                <button
-                    className="defaultBttn deleteAllBttn"
-                    onClick={() => setTasks([])}
-                >
-                    Delete All
-                </button>
-            </section>
 
-            <TaskList
-                tasks={workTasks}
-                deleteTask={deleteTask}
-                headerTitle={"Work List"}
-            />
-            <TaskList
-                tasks={homeTasks}
-                deleteTask={deleteTask}
-                headerTitle={"Home List"}
-            />
-            <TaskList
-                tasks={schoolTasks}
-                deleteTask={deleteTask}
-                headerTitle={"School List"}
-            />
-            <TaskList
-                tasks={otherTasks}
-                deleteTask={deleteTask}
-                headerTitle={"Other List"}
-            />
-        </section>
-    </>
-);
+                <TaskList
+                    tasks={workTasks}
+                    deleteTask={deleteTask}
+                    headerTitle={"Work List"}
+                />
+                <TaskList
+                    tasks={homeTasks}
+                    deleteTask={deleteTask}
+                    headerTitle={"Home List"}
+                />
+                <TaskList
+                    tasks={schoolTasks}
+                    deleteTask={deleteTask}
+                    headerTitle={"School List"}
+                />
+                <TaskList
+                    tasks={otherTasks}
+                    deleteTask={deleteTask}
+                    headerTitle={"Other List"}
+                />
+            </section>
+        </>
+    );
 }
